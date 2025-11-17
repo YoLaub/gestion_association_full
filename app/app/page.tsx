72dd9279cb3111d43d233e748c94b24a87d46a25
Julien Legoux <jlegoux@greta-bretagne-sud.fr>
2025-11-17 12:23:35 +0100

    Still test

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy Now
+            Deploy Now !!
           </a>
           <a
             className={styles.secondary}
