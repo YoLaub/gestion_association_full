254971929ec24387404d3fce8c44604ffc5a7ac9
YoLaub <johngreta904@gmail.com>
2026-01-14 22:52:17 +0100

    init develop

--- gestion-association-pwa/src/app/page.tsx
+++ gestion-association-pwa/src/app/page.tsx
@@ -1,34 +1,65 @@
-// src/app/page.tsx
-import { auth } from "@clerk/nextjs/server";
-import { RedirectToSignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
-// Clean Architecture imports
-import { GetDashboardDataUseCase } from "@/features/dashboard/application/useCases/GetDashboardDataUseCase";
-import { MockDashboardRepository } from "@/features/dashboard/infrastructure/repositories/MockDashboardRepository";
-import { DashboardView } from "@/features/dashboard/presentation/components/DashboardView";
-
-export default async function HomePage() {
-    // 1. Sécurité : Vérification Clerk (Côté Serveur)
-    const { userId } = await auth();
-
-     if (!userId) {
-         // Si pas connecté, on renvoie vers le login (ou on affiche une Landing Page)
-         return <RedirectToSignIn />;
-     }
-
-    // 2. Initialisation (Injection de dépendances)
-    // changer "Mock" par "Supabase" plus tard
-    const repository = new MockDashboardRepository();
-    const useCase = new GetDashboardDataUseCase(repository);
-
-    // 3. Exécution (Récupération des données pour cet utilisateur spécifique)
-    // au besoin : useCase.execute(userId)
-    const dashboardData = await useCase.execute();
-
-    // 4. Rendu
-    return (
-        <main>
-            <DashboardView data={dashboardData} />
-        </main>
-    );
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started to develop in branch develop, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
+  );
 }
